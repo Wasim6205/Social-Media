a1b69ec7c4591293718b047df5f51714d2124f0e
Wasim6205 <89179686+Wasim6205@users.noreply.github.com>
2025-08-05 12:16:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000
 
 app.use(cors({
     // origin: "http://localhost:5173",
-    origin: "https://instawave.onrender.com",
+    origin:"https://instawave.onrender.com",
     // origin: process.env.REACT_PUBLIC_BASE_URL,
     credentials: true,
 }))
